2da996c9171d47df634c1f52d0a308a577c98362
Pranshi Sahu <115640741+pranshisahu@users.noreply.github.com>
2023-10-17 17:10:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth()+1;
+var todayDate = String(date.getDate()).padStart(2,'0');
+var datePattern = year + '-' + '-' + todayDate
+document.getElementById("date-picker").value = datePattern;
